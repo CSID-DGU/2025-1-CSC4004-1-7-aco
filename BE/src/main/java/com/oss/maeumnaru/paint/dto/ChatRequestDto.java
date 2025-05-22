3b614d4704cb22e95b92dfb9525ce68f3162100c
minj0206 <alssl03@naver.com>
2025-05-22 15:19:46 +0900

    Feat: 1문-1답 형식으로 대화 저장 구현

--- BE/src/main/java/com/oss/maeumnaru/paint/dto/ChatRequestDto.java
+++ BE/src/main/java/com/oss/maeumnaru/paint/dto/ChatRequestDto.java
@@ -1,4 +1,10 @@
 package com.oss.maeumnaru.paint.dto;
 
-public class ChatRequestDto {
+import lombok.Getter;
+
+@Getter
+public class ChatRequestDto { //질문 - 응답 대화를 쌍으로 저장
+    private Long paintId;
+    private String chatbotComment;   // GPT 질문
+    private String patientComment;   // 사용자 응답
 }
