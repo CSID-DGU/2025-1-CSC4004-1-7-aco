0acd20dfa66032ab0894d882ffb9e588d4f92c2a
minj0206 <alssl03@naver.com>
2025-05-22 15:20:56 +0900

    Add: 그림 제목 필드 및 저장 기능 추가

--- BE/src/main/java/com/oss/maeumnaru/paint/dto/PaintRequestDto.java
+++ BE/src/main/java/com/oss/maeumnaru/paint/dto/PaintRequestDto.java
@@ -1,4 +1,12 @@
 package com.oss.maeumnaru.paint.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PaintRequestDto {
+    private Long patientCode;
+    private String title;
+
 }
