cf515b4f43f9ed66502c6c1447168349c32c7d6a
박혜린 <bokddang@dgu.ac.kr>
2025-05-30 15:15:45 +0900

    feat:로그인시 이름 반환 추가

--- BE/src/main/java/com/oss/maeumnaru/user/dto/response/TokenResponseDTO.java
+++ BE/src/main/java/com/oss/maeumnaru/user/dto/response/TokenResponseDTO.java
@@ -5,10 +5,11 @@ import com.oss.maeumnaru.user.entity.MemberEntity;
 public record TokenResponseDTO(
         String accessToken,
         String refreshToken,
-        MemberEntity.MemberType memberType
+        MemberEntity.MemberType memberType,
+        String name
 ) {
-    public static TokenResponseDTO of(String accessToken, String refreshToken, MemberEntity.MemberType memberType) {
-        return new TokenResponseDTO(accessToken, refreshToken, memberType);
+    public static TokenResponseDTO of(String accessToken, String refreshToken, MemberEntity.MemberType memberType, String name) {
+        return new TokenResponseDTO(accessToken, refreshToken, memberType, name);
     }
 }
 
