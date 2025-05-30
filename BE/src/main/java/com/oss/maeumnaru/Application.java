3fc603117caf35f72c3e49d522ad28a680ab30e2
Hyoseok Jang <lapaella1200422@gmail.com>
2025-05-31 03:17:05 +0900

    setting : set swagger

--- BE/src/main/java/com/oss/maeumnaru/Application.java
+++ BE/src/main/java/com/oss/maeumnaru/Application.java
@@ -1,11 +1,18 @@
 package com.oss.maeumnaru;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		servers = {
+				@Server(url="http://localhost:8080", description = "Default Server url")
+		}
+)
 public class Application {
 
 	public static void main(String[] args) {
