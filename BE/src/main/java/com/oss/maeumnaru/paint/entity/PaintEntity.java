314e2da8302eebc2b3acabba3733289b134fe8a3
minj0206 <alssl03@naver.com>
2025-06-02 02:49:18 +0900

    setter 관리

--- BE/src/main/java/com/oss/maeumnaru/paint/entity/PaintEntity.java
+++ BE/src/main/java/com/oss/maeumnaru/paint/entity/PaintEntity.java
@@ -12,38 +12,48 @@ import java.util.List;
 @Entity
 @Table(name = "paint")
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class PaintEntity {
 
     @Id
+    @Setter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long paintId;
 
+    @Setter
     private String fileUrl;
 
+    @Setter
     @Temporal(TemporalType.DATE)
     private String createDate;
 
+    @Setter
     @Temporal(TemporalType.TIMESTAMP)
     private Date updateDate;
 
+    @Setter
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "patient_code")
     private PatientEntity patient;
 
+    @Setter
     @OneToMany(mappedBy = "paint", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ChatEntity> chats = new ArrayList<>();
 
+    @Setter
     @Column(nullable = false)
     private String title;
 
+    @Setter
     private boolean finalized;
 
     @Column(nullable = false)
     private boolean chatCompleted = false;
 
+    public void setChatCompleted(boolean chatCompleted) {
+        this.chatCompleted = chatCompleted;
+    }
 
 }
