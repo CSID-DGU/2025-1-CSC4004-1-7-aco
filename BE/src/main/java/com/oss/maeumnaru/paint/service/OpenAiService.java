a3d3ef48649ac79f4d27671a90e9a93121b4a357
minj0206 <alssl03@naver.com>
2025-06-06 19:22:54 +0900

    fix: 챗봇 질문 글자수 제한, 모델 변경, 프롬프트 개선 5

--- BE/src/main/java/com/oss/maeumnaru/paint/service/OpenAiService.java
+++ BE/src/main/java/com/oss/maeumnaru/paint/service/OpenAiService.java
@@ -27,7 +27,7 @@ public class OpenAiService {
 
         JSONObject message1 = new JSONObject()
                 .put("role", "system")
-                .put("content", "당신은 감정 분석 상담사입니다. 채팅창 옆에는 사용자가 그린 그림이 띄워져있고 당신은 이 그림에 관해 사용자와 대화할 것입니다. 사용자의 응답에 따라 사용자의 감정과 그림의 의미를 더 깊이 탐색할 수 있는 질문을 해주세요.");
+                .put("content", "당신은 감정 분석 상담사입니다. 채팅창 옆에는 사용자가 그린 그림이 띄워져있고 당신은 이 그림에 관해 사용자와 대화할 것입니다. 사용자의 응답에 따라 사용자의 감정과 그림의 의미를 더 깊이 탐색할 수 있는 질문을 해주세요. 너무 길게 설명하지 말하지 말고 두 세문장으로만 대답해주세요.");
 
         JSONObject message2 = new JSONObject()
                 .put("role", "user")
@@ -38,10 +38,10 @@ public class OpenAiService {
                 .put(message2);
 
         JSONObject body = new JSONObject()
-                .put("model", "o4-mini")
+                .put("model", "gpt-4o")
                 .put("messages", messages)
-                .put("max_completion_tokens", 20)
-                .put("reasoning_effort", "medium");
+                .put("max_tokens", 70)
+                .put("temperature", 0.7);
 
         Request request = new Request.Builder()
                 .url(API_URL)
