fcc727c8360da5de982b33616d5b29c1f80ac1b2
박혜린 <bokddang@dgu.ac.kr>
2025-06-06 15:11:45 +0900

    feat:한달치(달력 표시용) emotionrate 넘겨주기

--- BE/src/main/java/com/oss/maeumnaru/diary/service/EmotionService.java
+++ BE/src/main/java/com/oss/maeumnaru/diary/service/EmotionService.java
@@ -1,6 +1,6 @@
 package com.oss.maeumnaru.diary.service;
 
-import com.oss.maeumnaru.diary.dto.DiaryResponseDto;
+import com.oss.maeumnaru.diary.dto.EmotionResponseDto;
 import com.oss.maeumnaru.diary.entity.DiaryEntity;
 import com.oss.maeumnaru.diary.repository.DiaryRepository;
 import com.oss.maeumnaru.global.error.exception.ApiException;
@@ -18,14 +18,14 @@ public class EmotionService {
 
     private final DiaryRepository diaryRepository;
 
-    // 연-월 조회만 사용
-    public List<DiaryResponseDto> getDiariesByPatientCodeAndMonth(String patientCode, String year, String month) {
+    public List<EmotionResponseDto> getEmotionRatesByPatientCodeAndMonth(String patientCode, String year, String month) {
         try {
             List<DiaryEntity> diaries = diaryRepository
                     .findByPatient_PatientCodeAndYearAndMonth(patientCode, year, month);
 
             return diaries.stream()
-                    .map(DiaryResponseDto::fromEntity)
+                    .filter(diary -> diary.getDiaryAnalysis() != null)
+                    .map(diary -> EmotionResponseDto.fromEntity(diary.getDiaryAnalysis()))
                     .collect(Collectors.toList());
         } catch (DataAccessException e) {
             throw new ApiException(ExceptionEnum.DATABASE_ERROR);
